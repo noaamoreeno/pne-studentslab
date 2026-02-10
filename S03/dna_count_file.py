ecc1c5b84e3a58bad510ffc547a276dcf5b7202f
noaamoreeno <n.morenom.2025@alumnos.urjc.es>
2026-02-10 16:52:12 +0100

    exercise 3: count dna bases with the file

--- S03/dna_count_file.py
+++ S03/dna_count_file.py
@@ -1,16 +1,31 @@
-lines = ["AGTACACTGGT", "ACCAGTGTACT", "ATGGCCATTGTAATGGGCCGCTGAAAGGGTGCCCGATAG"]
+#It is correct but the lines are one basis as a whole, not 3 different ones
+#reading files
+#lines=["AGTACACTGGT", "ACCAGTGTACT", "ATGGCCATTGTAATGGGCCGCTGAAAGGGTGCCCGATAG"]
+#print("From variable:", lines)
 
-def count_bases():
-    for sequence in lines:
-        info = {"Length": len(sequence), "A": 0, "C": 0, "G": 0, "T": 0}
+from dna_count import count_bases #import functions from other codes
 
-        for base in sequence:
-            if base in info:
-                info[base] += 1
+f = open("dna.txt", "r") #r means we are reading
+#Here we put the code
+lines = f.readlines()
+f.close() #If you forget to put the close function the file could corrupt when working in other terminals
 
-        print("Sequence:", sequence)
-        for base, count in info.items():
-            print(f'{base}:{count}')
+#another way of opening files
+with open("dna.txt", "r") as f: #with this you don't need to use the close function
+    lines = f.readlines()
+
+total_number = 0
+bases = {"A": 0, "C": 0, "G": 0, "T": 0}
+
+for sequence in lines:
+    sequence = sequence.strip() #Remove spaces and newline characters at the end of the string
+    total_number += len(sequence)
+
+    result = count_bases(sequence)
+
+print("Total number of bases:", total_number)
+
+for base, count in bases.items():
+    print(f'{base}:{count}')
 
 
-count_bases()
