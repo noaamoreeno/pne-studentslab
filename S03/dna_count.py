03b1bbd69f2b3bf69b5c1d177f910ca74a4a6406
noaamoreeno <n.morenom.2025@alumnos.urjc.es>
2026-02-10 16:51:48 +0100

    exercise 2: count dna bases with the __name__ update

--- S03/dna_count.py
+++ S03/dna_count.py
@@ -1,6 +1,3 @@
-sequence = input("Introduce the sequence: ")
-print("Length:", len(sequence))
-
 def count_bases1():
     a = 0
     c = 0
@@ -21,17 +18,18 @@ def count_bases1():
     print("T:", t)
 
 #another method
-def count_bases2():
+def count_bases(sequence):
     bases = {"A": 0, "C": 0, "G": 0, "T": 0}
 
     for base in sequence:
         if base in bases:
             bases[base]+=1
+    return bases
 
-    print(bases)
+if __name__ == "__main__":
+    sequence = input("Introduce the sequence: ")
+    print("Length:", len(sequence))
 
-def count_bases3():
-    bases = {"A": 0, "C": 0, "G": 0, "T": 0}
-
-    for base, count in bases.items():
+    result = count_bases(sequence)
+    for base, count in result.items():
         print(f"{base}:{count}")
