adac4999470dbe465724f01dcfefa93bcd131342
noaamoreeno <n.morenom.2025@alumnos.urjc.es>
2026-02-11 18:22:44 +0100

    basic exercises

---
+++ basic_exercises/exercise5-loops.py
@@ -0,0 +1,13 @@
+words = ["Python", "is", "a", "programming", "language"]
+
+print("--- Exercise 5a ---")
+for i in words:
+    print(i,"->", len(i), "characters")
+
+print("--- Exercise 5b ---")
+n = 1
+print(n)
+while n <= 1000:
+    n = n * 2
+    print(n)
+
