f2764bde898d5d797b38182a1ce8702a729299ce
noaamoreeno <n.morenom.2025@alumnos.urjc.es>
2026-02-11 18:43:44 +0100

    basic exercises

---
+++ basic_exercises/exercise7-dictionaries.py
@@ -0,0 +1,21 @@
+student = {
+    "name": "Carlos",
+    "age": 22,
+    "subjects": ["PNE", "Networks", "Databases"],
+    "grades": {"PNE": 8.5, "Networks": 7.0, "Databases": 9.2}
+}
+
+count = 0
+for i in student["grades"].values():
+    count += i
+    avg = round(count / len(student["grades"]), 2)
+
+
+
+def main():
+    print("Name:", student["Name"])
+    print("Number of subjets:", len(student["subjects"]))
+    print("Enrolled in PNE:", "PNE" in student["subjects"])
+    print("Databases grade:", student["grades"]["Databases"])
+    print("Average grade: ", avg)
+
