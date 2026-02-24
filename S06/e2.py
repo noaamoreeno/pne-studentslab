28c9eba9fa0d879d7f50369719f42007cfd870ef
noaamoreeno <n.morenom.2025@alumnos.urjc.es>
2026-02-24 16:41:57 +0100

    session 4

---
+++ S06/e2.py
@@ -0,0 +1,19 @@
+class Seq:
+    """A class for representing DNA sequences"""
+    def __init__(self, strbases):
+        valid_bases = "ACGT"
+
+        for base in strbases:
+            if base not in valid_bases:
+                raise ValueError("Incorrect sequence detected")
+        self.strbases = strbases
+    def __str__(self):
+        return self.strbases
+
+def print_seqs(seq_list):
+    for index, seq in enumerate(seq_list):
+        print(f"Sequence {index}: (Length: {len(seq.strbases)}) {seq}")
+
+# Example
+seq_list = [Seq("ACT"), Seq("GATA"), Seq("CAGATA")]
+print_seqs(seq_list)
